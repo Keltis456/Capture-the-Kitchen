716c9c4587127f6120b33aa894afab4cf45a8871
Keltis456 <svet_svb@mail.ru>
2018-03-08 16:30:19 +0200

    Удалены 3д модели. Добавлен алгоритм отслеживания клика по гексу. Добавлена генерация карты.

---
+++ Assets/Hex.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex : MonoBehaviour {
+
+    SpriteRenderer cellRenderer;
+    GameObject graphics;
+    Color defaultColor;
+    Color currColor;
+    public Color onMouseEnterColor = Color.HSVToRGB(0, 0.5f, 1);
+    public Color onMouseDownColor = Color.HSVToRGB(0, 1, 1);
+
+	// Use this for initialization
+	void Start () {
+        graphics = transform.Find("Graphics").gameObject;
+        cellRenderer = graphics.GetComponentInChildren<SpriteRenderer>();
+        defaultColor = cellRenderer.color;
+        currColor = defaultColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        cellRenderer.color = onMouseEnterColor;
+    }
+
+    private void OnMouseExit()
+    {
+        cellRenderer.color = currColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (currColor != defaultColor)
+        {
+            currColor = defaultColor;
+            cellRenderer.color = currColor;
+        }
+        else
+        {
+            currColor = onMouseDownColor;
+            cellRenderer.color = currColor;
+        }
+    }
+}
---
+++ Assets/MapAssembler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapAssembler : MonoBehaviour {
+
+    public GameObject hexagonal_cell;
+    public Vector2 sizeOfMap = new Vector2(1,1);
+    Vector2 startPos;
+    
+
+	void Start () {
+        startPos = transform.position;
+        
+        for (int i = 0; i <= sizeOfMap.x; i++)
+        {
+            for (int j = 0; j <= sizeOfMap.y; j++)
+            {
+                Instantiate(hexagonal_cell, new Vector2(startPos.x + j * 3.84f, startPos.y + i*4.43f - j * 2.215f), Quaternion.identity);
+            }
+        }
+        
+    }
+}
