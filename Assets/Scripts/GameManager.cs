9356c4d62fd92643a8e59a885b03560c6c5ce920
Nikita <keltis.kk@gmail.com>
2018-04-16 15:54:47 +0300

    normalizedTime animation fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour {
     public GameObject playerEthalon;
     public Canvas canvas;
 
+    
+
     #region Singleton
     public static GameManager instance;
 
@@ -33,6 +35,8 @@ public class GameManager : MonoBehaviour {
             item.currAbleSteps = 0;
             //item.gameObject.GetComponent<Animation>().Play("Unit_Idle");
             //item.gameObject.GetComponent<Animation>()["Unit_Idle"]
+
+            item.tmpAnimNormTime = item.gameObject.GetComponent<Animation>()["Unit_Idle"].normalizedTime;
             item.gameObject.GetComponent<Animation>().Stop();
         }
         if (players.IndexOf(currActivePlayer) + 1 >= players.Count)
@@ -47,6 +51,7 @@ public class GameManager : MonoBehaviour {
         {
             item.currAbleSteps = item.maxAbleSteps;
             item.gameObject.GetComponent<Animation>().Play("Unit_Idle");
+            item.gameObject.GetComponent<Animation>()["Unit_Idle"].normalizedTime = item.tmpAnimNormTime;
         }
         Debug.Log("CurrActivePlayer = " + players.IndexOf(currActivePlayer));
     }
