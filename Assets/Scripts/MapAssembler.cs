3f4db155e9a6d6aa8dbdb7ebf44f1ccca8f0da97
Nikita <keltis.kk@gmail.com>
2018-04-25 19:48:08 +0300

    New cell prefabs, map assembler update

--- Assets/Scripts/MapAssembler.cs
+++ Assets/Scripts/MapAssembler.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapAssembler : MonoBehaviour {
-
-    public GameObject hexagonal_cell;
+    
+    public GameObject[] cell_prefabs;
     public Vector2 sizeOfMap = new Vector2(1,1);
     Vector2 startPos;
     
@@ -15,11 +15,16 @@ public class MapAssembler : MonoBehaviour {
         {
             for (int j = 0; j < sizeOfMap.y; j++)
             {
-                GameObject hex = Instantiate(hexagonal_cell, new Vector2(startPos.x + j * 3.84f, startPos.y + i*4.43f - j * 2.215f), Quaternion.identity);
+                GameObject hex = Instantiate(getRandomCellPrefab(), new Vector2(startPos.x + j * 3.84f, startPos.y + i*4.43f - j * 2.215f), Quaternion.identity);
                 Map.instance.hices[i,j] = (hex.GetComponent<Hex>());
                 hex.GetComponent<Hex>().posAtMap = new Vector2(i, j);
                 hex.transform.SetParent(Map.instance.transform);
             }
         }
+    }
+
+    GameObject getRandomCellPrefab()
+    {
+        return cell_prefabs[Random.Range(0, cell_prefabs.Length)];
     }
 }
